09b8af121f396a8a3524615f7c467970d3f3c2f3
jiazhi <platypuspanini@gmail.com>
2022-02-09 22:00:34 +0800

    Update based on review

--- platypus.js
+++ platypus.js
@@ -256,7 +256,7 @@ function render () {
 
 /* Timer */
 function makeTimer() {
-    var endTime = new Date("February 22, 2022 00:00:00 PDT");
+    var endTime = new Date("March 5, 2022 00:00:00 PDT");
     var endTime = (Date.parse(endTime)) / 1000;
     var now = new Date();
     var now = (Date.parse(now) / 1000);
@@ -266,15 +266,15 @@ function makeTimer() {
     var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
     var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
     
-    var hours = Math.floor(timeLeft / 3600);
+    // var hours = Math.floor(timeLeft / 3600);
     
     if (hours < "10") { hours = "0" + hours; }
     if (minutes < "10") { minutes = "0" + minutes; }
     if (seconds < "10") { seconds = "0" + seconds; }
-    // $("#days").html(days);
+    $("#days").html(days);
     $("#hours").html(hours);
     $("#minutes").html(minutes);
-    $("#seconds").html(seconds);
+    // $("#seconds").html(seconds);
  }
 
  setInterval(function() { makeTimer(); }, 0);
